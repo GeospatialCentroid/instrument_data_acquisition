51b1022889eb364ce08bde2e847874adb182d061
kevinworthington <kevin.a.worthington@gmail.com>
2024-12-04 10:48:54 -0700

    added main.py and gitignore

---
+++ main.py
@@ -0,0 +1,20 @@
+import serial
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+working_serials=[]
+for port, desc, hwid in sorted(ports):
+    print("checking ",port)
+    try:
+        ser = serial.Serial(port=port, baudrate=2400)
+        print("{}: {} [{}]".format(port, desc, hwid), "works")
+        working_serials.append(ser)
+    except:
+        pass
+print ("watching",working_serials)
+while True:
+    for i in working_serials:
+        try:
+            value=i.readline()
+            print(str(value))
+        except:
+            pass
